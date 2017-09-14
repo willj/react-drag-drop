103800ed43325378986fad4507c9dcd9b9b037d0
Will <willj@users.noreply.github.com>
2017-09-14 18:10:48 +0100

    Begin file dropping...

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -12,6 +12,9 @@ class Grid extends React.Component {
 
         this.dragStarted = this.dragStarted.bind(this);
         this.itemDropped = this.itemDropped.bind(this);
+        this.fileDropped = this.fileDropped.bind(this);
+        this.gridDragEnd = this.gridDragEnd.bind(this);
+        this.gridDragOver = this.gridDragOver.bind(this);
     }
 
     // you can't access an item being dragged from the dragOver event, only the target, so lets keep track of what's being dragged
@@ -19,7 +22,26 @@ class Grid extends React.Component {
         this.setState({ currentDragIndex: dragIndex });
     }
 
+    gridDragEnd(e){
+        e.preventDefault();
+        e.dataTransfer.clearData();
+    }
+
+    gridDragOver(e){
+        e.preventDefault();
+    }
+
     itemDropped(e, dropzoneIndex){
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length > 0) {
+            return;
+        }
+
+        e.stopPropagation();
+        
+        console.log("item dropped");
+
         let itemIndex = e.dataTransfer.getData("text/plain");
         
         let items = [...this.state.items];
@@ -30,9 +52,18 @@ class Grid extends React.Component {
         this.setState({items: items});
     }
 
+    fileDropped(e){
+        e.preventDefault();
+        console.log("file dropped");
+        console.log(e.dataTransfer.files);
+    }
+
     render(){
         return (
-            <div className="grid">
+            <div className="grid" 
+                onDrop={this.fileDropped} 
+                onDragEnd={this.gridDragEnd} 
+                onDragOver={this.gridDragOver}>
                 {
                     this.state.items.map((item, index) => {
                         return (
