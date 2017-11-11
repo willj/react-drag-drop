ed51fd2a9d687a36e5937bbdcd419725451579dc
Will <willj@users.noreply.github.com>
2017-11-11 16:27:33 +0000

    Add preview image

--- src/components/EditableItem.js
+++ src/components/EditableItem.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 
 const Image = ({item}) => {
     return (
-        <p>I'm an Image called {item.title}</p>
+        <div>
+            <p>I'm an Image called {item.title}</p>
+            <img src={item.url} alt={item.title} style={{ width: '100%' }} />
+        </div>
     );
 };
 
