08962ca5b41935407a48210723170c05df7aba98
Will <willj@users.noreply.github.com>
2017-11-25 16:15:57 +0000

    Add basename on router so github pages works

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Switch>
                     <Route exact path="/" component={Project} />
                     <Route path="/view/:id/:itemIndex?" component={Viewer} />
